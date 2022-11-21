8cc73bfd8facbb05bdcb48fb377b494c1c430d47
bainchild <55368789+bainchild@users.noreply.github.com>
2022-11-20 23:32:36 -0600

    Create PlatformTypes.lua

---
+++ src/Lua51/PlatformTypes.lua
@@ -0,0 +1,33 @@
+return {  
+    ["x86 standard"] = {    
+        Description = "x86 Standard (32-bit, little endian, double)",    
+        BigEndian = false,             -- 1 = little endian    
+        IntegerSize = 4,                -- (data type sizes in bytes)    
+        SizeT = 4,    
+        InstructionSize = 4,    
+        NumberSize = 8,                 -- this & integral identifies the    
+        IsFloatingPoint = true,        -- (0) type of lua_Number    
+        NumberType = "double",         -- used for lookups  
+    },  
+    ["big endian int"] = {    
+        Description = "(32-bit, big endian, int)",    
+        BigEndian = true,
+        IntegerSize = 4,
+        SizeT = 4,
+        InstructionSize = 4,
+        NumberSize = 4,
+        IsFloatingPoint = false,
+        NumberType = "int",  
+    },  
+    ["amd64"] = {    
+        Description = "AMD64 (64-bit, little endian, double)",    
+        BigEndian = false, 
+        IntegerSize = 4,    
+        SizeT = 8,    
+        InstructionSize = 4,    
+        NumberSize = 8,    
+        IsFloatingPoint = true,
+        NumberType = "double",  
+    },  
+    -- you can add more platforms here
+}
